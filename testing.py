40c43bddd7bc3f27b895d46ba0ddd00755aed0e4
achrysaetos <achrysaetos@yahoo.com>
2023-02-25 05:13:04 -0500

    refactor

--- testing.py
+++ testing.py
@@ -14,47 +14,41 @@ def calc_return(file, start, end): # dates of investing, inclusive
 
 
 def main():
-    # directory = "s&p500"; file = "s&p500/AAPL.csv"
-    # files = []
-    # for filename in os.scandir(directory):
-    #     if filename.is_file():
-    #         files.append(filename.path)
-    file = "combined.csv"
-
-    # according to dollar cost averaging
-    default_portfolio, lstm_portfolio = 0, 0
-    correct, incorrect = 0, 0
+    file = "combined.csv" # location of data
+    df = pd.read_csv(file)
 
     # choose a window and a number of time steps
     seq_size, n_steps = 52, 4
     # choose a batch size and a number of epochs
     batch_size, num_epochs = 50, 200
-    # define input sequence
-    df = pd.read_csv(file)
+
+    # organize data
     raw_seq = df["vix"].tolist()
-    log_ret = df["s&p(log)"].tolist()
-    data = log_ret[seq_size:]
+    log_ret = df["s&p(log)"].tolist()[seq_size:]
     week = df["Date"].tolist()[seq_size:]
     default_log_ret, lstm_log_ret = [], []
+    default_portfolio, lstm_portfolio = 0, 0
+    correct, incorrect = 0, 0
+
     # split into samples and run model
     for i in range(len(raw_seq)-seq_size):
         inputs, outputs = split_sequence_univariate(raw_seq[i:seq_size+i], n_steps)
         # prediction = raw_seq[i:seq_size+i+1][-1]
         prediction = univariate(inputs, outputs, raw_seq[i:seq_size+i], n_steps, batch_size, num_epochs, type="vanilla")
-        default_log_ret.append(data[i])
+        default_log_ret.append(log_ret[i])
         default_portfolio = 2.718281828459**sum(default_log_ret)
         # print(f"Prev: {raw_seq[i:seq_size+i][-1]}, Prediction: {prediction}, Actual: {raw_seq[i:seq_size+i+1][-1]}")
-        # print(f"Week: {week[i]}, Data: {data[i]}, Focus: {raw_seq[i:seq_size+i+1][-1]}")
+        # print(f"Row: {seq_size+i+2}, Week: {week[i]}, Return: {log_ret[i]}, Focus: {raw_seq[i:seq_size+i+1][-1]}")
         if prediction < raw_seq[i:seq_size+i][-1]:
-            lstm_log_ret.append(data[i])
+            lstm_log_ret.append(log_ret[i])
             lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
-            print(f"Buy on {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
+            print(f"Buy on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
             if raw_seq[i:seq_size+i+1][-1] < raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
             else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
         else:
             lstm_log_ret.append(0)
             lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
-            print(f"Sell on {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
+            print(f"Sell on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
             if raw_seq[i:seq_size+i+1][-1] > raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
             else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
         
