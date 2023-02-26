acecfb9ac1da7223d17932ef51ca6322eae04342
achrysaetos <achrysaetos@yahoo.com>
2023-02-25 20:58:48 -0500

    lstm vs passive on vix

--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
-import numpy as np
 import pandas as pd
-import os
+import csv
 from lstm.univariate import univariate, split_sequence_univariate
 
 
@@ -22,26 +21,29 @@ def main():
     correct, incorrect = 0, 0
 
     # split into samples and run model
-    for i in range(len(raw_seq)-seq_size):
-        inputs, outputs = split_sequence_univariate(raw_seq[i:seq_size+i], n_steps)
-        # prediction = raw_seq[i:seq_size+i+1][-1]
-        prediction = univariate(inputs, outputs, raw_seq[i:seq_size+i], n_steps, batch_size, num_epochs, type="vanilla")
-        default_log_ret.append(log_ret[i])
-        default_portfolio = 2.718281828459**sum(default_log_ret)
-        # print(f"Prev: {raw_seq[i:seq_size+i][-1]}, Prediction: {prediction}, Actual: {raw_seq[i:seq_size+i+1][-1]}")
-        # print(f"Row: {seq_size+i+2}, Week: {week[i]}, Return: {log_ret[i]}, Focus: {raw_seq[i:seq_size+i+1][-1]}")
-        if prediction < raw_seq[i:seq_size+i][-1]:
-            lstm_log_ret.append(log_ret[i])
-            lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
-            print(f"Buy on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
-            if raw_seq[i:seq_size+i+1][-1] < raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
-            else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
-        else:
-            lstm_log_ret.append(0)
-            lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
-            print(f"Sell on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
-            if raw_seq[i:seq_size+i+1][-1] > raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
-            else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Week", "Date", "LSTM", "Passive", "Correct"])
+        for i in range(len(raw_seq)-seq_size):
+            inputs, outputs = split_sequence_univariate(raw_seq[i:seq_size+i], n_steps)
+            # prediction = raw_seq[i:seq_size+i+1][-1]
+            prediction = univariate(inputs, outputs, raw_seq[i:seq_size+i], n_steps, batch_size, num_epochs, type="vanilla")
+            default_log_ret.append(log_ret[i])
+            default_portfolio = 2.718281828459**sum(default_log_ret)
+            # print(f"Prev: {raw_seq[i:seq_size+i][-1]}, Prediction: {prediction}, Actual: {raw_seq[i:seq_size+i+1][-1]}")
+            if prediction < raw_seq[i:seq_size+i][-1]:
+                lstm_log_ret.append(log_ret[i])
+                lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
+                print(f"Buy on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
+                if raw_seq[i:seq_size+i+1][-1] < raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
+                else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
+            else:
+                lstm_log_ret.append(0)
+                lstm_portfolio = 2.718281828459**sum(lstm_log_ret)
+                print(f"Sell on ({seq_size+i+2}) {week[i]} => {round(lstm_portfolio, 2)} vs {round(default_portfolio, 2)}", end=" ")
+                if raw_seq[i:seq_size+i+1][-1] > raw_seq[i:seq_size+i][-1]: correct += 1; print(f"(Correct x{correct}, {round(correct*100/(correct+incorrect), 1)}%)")
+                else: incorrect += 1; print(f"(Incorrect x{incorrect}, {round(incorrect*100/(correct+incorrect), 1)}%)")
+            writer.writerow([seq_size+i+2, week[i], round(lstm_portfolio, 3), round(default_portfolio, 3), round(correct/(correct+incorrect), 3)])
         
 
 if __name__ == "__main__":
